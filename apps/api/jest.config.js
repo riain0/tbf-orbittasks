2e269f53a2868f64bc5b2814c2c07596c71b1be0
Riain Condon <condon.riain@gmail.com>
2026-07-17 11:17:24 -0400

    fix: jest types

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -15,6 +15,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
+  },
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
 
